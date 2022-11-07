db07c66bf50b3b6529acb7e79a77fb72af03f1a9
diddone <dmpozdeev@edu.hse.ru>
2022-11-07 05:22:37 +0300

    Probbaly final commit

--- stream.py
+++ stream.py
@@ -7,6 +7,85 @@ from torchaudio.io import StreamReader
 logger = logging.getLogger(__file__)
 
 
+class Streaming(nn.Module):
+    def __init__(self, model, melspec, window_size=7500, step_size=3750, is_half=True):
+        super().__init__()
+
+
+        self.window_size = window_size
+        self.step_size = step_size
+
+        self.is_half = is_half
+        self.model = model if not is_half else model.half()
+        config = self.model.config
+        self.device = config.device
+
+        self.melspec = torchaudio.transforms.MelSpectrogram(
+                sample_rate=config.sample_rate,
+                n_fft=400,
+                win_length=400,
+                hop_length=160,
+                n_mels=config.n_mels
+        ).to(config.device)
+
+
+        self.buffer = torch.zeros([1, 0])
+        self.gru_outputs = torch.zeros([1, 0, 1]).to(self.device)
+        self.hidden = torch.zeros([1, 0]).to(self.device)
+
+        self.prev_proba = 0.
+
+    def forward(self, chunk):
+        assert len(chunk.size()) == 2
+        max_proba = 0.
+
+        if self.buffer.size()[-1] < self.window_size:
+            need_len = self.window_size - self.buffer.size()[-1]
+
+            self.buffer = torch.cat([self.buffer, chunk[:, :need_len]], dim=1)
+            chunk = chunk[:, need_len:]
+
+            if self.buffer.size()[-1] == self.window_size:
+                max_proba = self.model_forward(self.buffer)
+            else:
+                return self.prev_proba
+
+        chunks = torch.split(chunk, self.step_size, dim=-1)
+        for chunk in chunks:
+
+            self.buffer = torch.cat([self.buffer[:, self.step_size:], chunk], dim=1)
+
+            if self.buffer.size()[-1] == self.window_size:
+                cur_proba = self.model_forward(self.buffer)
+                max_proba = max(max_proba, cur_proba)
+
+        self.prev_proba = max_proba
+        return max_proba
+
+
+    def model_forward(self, x):
+
+        if self.gru_outputs is not None:
+            x = x[:, -self.step_size:]
+
+        x = x.to(self.device)
+        x = torch.log(self.melspec(x).clamp_(min=1e-9, max=1e9))
+        x = x if not self.is_half else x.half()
+        input = x.unsqueeze(dim=1)
+        conv_output = self.model.conv(input).transpose(-1, -2)
+
+        hidden = None if self.hidden.size()[-1] == 0 else self.hidden
+        gru_outputs, hidden = self.model.gru(conv_output, hidden)
+        self.hidden = hidden
+
+        if self.gru_outputs.size()[1] != 0:
+            gru_outputs = torch.cat([self.gru_outputs[:, self.step_size:, :], gru_outputs], dim=1)
+
+        self.gru_outputs = gru_outputs
+
+        contex_vector = self.model.attention(gru_outputs)
+        output = self.model.classifier(contex_vector)
+        return F.softmax(output, dim=-1)[0][1].item()
 
 
 def audio_stream(queue: mp.Queue):
@@ -16,7 +95,7 @@ def audio_stream(queue: mp.Queue):
     """
 
     streamer = StreamReader(src=":0", format="avfoundation")
-    streamer.add_basic_audio_stream(frames_per_chunk=8000, sample_rate=16000)
+    streamer.add_basic_audio_stream(frames_per_chunk=7500, sample_rate=16000)
     stream_iterator = streamer.stream(-1, 1)
 
     logger.info("Start audio streaming")
@@ -32,13 +111,13 @@ if __name__ == "__main__":
     args.add_argument(
             "-c",
             "--ckpt",
-            default='kws.pth',
+            default='stream_jit.pt',
             type=str,
             help="model file path (default: kws.pth)",
         )
 
     args = args.parse_args()
-    model = torch.load(args.ckpt).eval()
+    model = torch.jit.load(args.ckpt).eval()
 
     ctx = mp.get_context("spawn")
     chunk_queue = ctx.Queue()
